9b4a1dcc911b974ba501a75f59d0d9ed712646ef
Muhammad Anjum Kaiser <anjumbutt@gmail.com>
2020-04-22 22:36:47 +0500

    Initial Commit

---
+++ Miracle Pills Storyboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Miracle Pills Storyboard
+//
+//  Created by Muhammad Anjum Kaiser on 22/04/2020.
+//  Copyright © 2020 Muhammad Anjum Kaiser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

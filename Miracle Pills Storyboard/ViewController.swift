bd23085e414d1fa8dcd97c85474fff9aa771f08f
Muhammad Anjum Kaiser <anjumbutt@gmail.com>
2020-04-23 01:56:28 +0500

    added state picker, and coded delegate and datasource

--- Miracle Pills Storyboard/ViewController.swift
+++ Miracle Pills Storyboard/ViewController.swift
@@ -8,13 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var stateButton: UIButton!
+    
+    let states = ["Alaska", "California", "Maine", "New york"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        statePicker.dataSource = self
+        statePicker.delegate = self
     }
-
+    
+    
+    @IBAction func stateButton_Pressed(_ sender: Any) {
+        statePicker.isHidden = false
+    }
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        stateButton.setTitle(states[row], for: UIControl.State.normal)
+        statePicker.isHidden = true
+    }
 
 }
 
